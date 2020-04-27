a509a8f92642d70a41765f9ff46341291303d2cc
Nina Markovic <mnina5580@gmail.com>
2020-04-27 22:45:00 +0200

    Definisane metode, login

--- app/src/main/java/com/example/androidtim4/EmailActivity.java
+++ app/src/main/java/com/example/androidtim4/EmailActivity.java
@@ -11,4 +11,28 @@ public class EmailActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_email);
     }
+    @Override
+    public void onStart() {
+        super.onStart();
+    }
+    @Override
+    public void onRestart() {
+        super.onRestart();
+    }
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+    @Override
+    public void onPause() {
+        super.onPause();
+    }
+    @Override
+    public void onStop() {
+        super.onStop();
+    }
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
 }
