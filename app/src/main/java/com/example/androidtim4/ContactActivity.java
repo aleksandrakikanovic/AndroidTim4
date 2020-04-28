9af71cfed782ed0e28fd7d1d79c7f2895ed9a979
joka78 <j_subotic@yahoo.com>
2020-04-28 16:00:10 +0200

    Napravljene 4 aktivnosti : profile,contacts,createContact i contact.Dodata i podesena slika u Splash aktivnost.

---
+++ app/src/main/java/com/example/androidtim4/ContactActivity.java
@@ -0,0 +1,38 @@
+package com.example.androidtim4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ContactActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contact);
+    }
+    @Override
+    public void onStart() {
+        super.onStart();
+    }
+    @Override
+    public void onRestart() {
+        super.onRestart();
+    }
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+    @Override
+    public void onPause() {
+        super.onPause();
+    }
+    @Override
+    public void onStop() {
+        super.onStop();
+    }
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+}
---
+++ app/src/main/java/com/example/androidtim4/ProfileActivity.java
@@ -0,0 +1,38 @@
+package com.example.androidtim4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
+    @Override
+    public void onStart() {
+        super.onStart();
+    }
+    @Override
+    public void onRestart() {
+        super.onRestart();
+    }
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+    @Override
+    public void onPause() {
+        super.onPause();
+    }
+    @Override
+    public void onStop() {
+        super.onStop();
+    }
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+}
